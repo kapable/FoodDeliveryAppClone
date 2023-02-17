f4a57639fed9c70365a223d275cf16f0a3a5cf09
kapable <skjoldenstore@gmail.com>
2023-02-17 13:09:29 +0900

    initial structure setting

---
+++ src/pages/Complete.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function Complete() {
+  return (
+    <View>
+      <Text>Complete</Text>
+    </View>
+  );
+}
