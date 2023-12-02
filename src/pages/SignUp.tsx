2f87ebded86be3383e4d0caf4a255a82b13cc3a3
kapable <skjoldenstore@gmail.com>
2023-12-02 16:59:47 +0900

    fix setting for android config

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -62,7 +62,6 @@ function SignUp({navigation}: SignUpScreenProps) {
         '비밀번호는 영문,숫자,특수문자($@^!%*#?&)를 모두 포함하여 8자 이상 입력해야합니다.',
       );
     }
-    console.log(email, name, password);
     try {
       setLoading(true);
       const response = await axios.post(`${Config.DEV_API_URL}/user`, {
@@ -71,6 +70,7 @@ function SignUp({navigation}: SignUpScreenProps) {
         password,
       });
       Alert.alert('알림', '회원가입 되었습니다.');
+      navigation.navigate('SignIn');
     } catch (error) {
       const errorResponse = (error as AxiosError<{message: string}>).response;
       console.log(errorResponse);
@@ -80,7 +80,7 @@ function SignUp({navigation}: SignUpScreenProps) {
     } finally {
       setLoading(false);
     }
-  }, [email, name, password, loading]);
+  }, [navigation, email, name, password, loading]);
 
   const toSignIn = useCallback(() => {
     navigation.navigate('SignIn');
