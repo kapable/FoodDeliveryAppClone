ee70a3fe54020f667f766684a8484797b3e4ec6e
kapable <skjoldenstore@gmail.com>
2023-12-02 21:46:47 +0900

    setting socket for logout

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,10 +1,74 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import axios, {AxiosError} from 'axios';
+import React, {useCallback} from 'react';
+import {View, Text, StyleSheet, Pressable, Alert} from 'react-native';
+import Config from 'react-native-config';
+import {useAppDispatch} from '../store';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/reducer';
+import userSlice from '../slices/user';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 export default function Settings() {
+  const dispatch = useAppDispatch();
+  const accessToken = useSelector((state: RootState) => state.user.accessToken);
+  const onLogout = useCallback(async () => {
+    try {
+      await axios.post(
+        `${Config.DEV_API_URL}/logout`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      );
+      Alert.alert('알림', '로그아웃 되었습니다.');
+      dispatch(
+        userSlice.actions.setUser({
+          name: '',
+          email: '',
+          accessToken: '',
+        }),
+      );
+      await EncryptedStorage.removeItem('refreshToken');
+    } catch (error) {
+      const errorResponse = (error as AxiosError).response;
+      console.error(errorResponse);
+    }
+  }, [dispatch, accessToken]);
   return (
     <View>
-      <Text>설정</Text>
+      <View style={styles.buttonZone}>
+        <Pressable
+          style={StyleSheet.compose(
+            styles.loginButton,
+            styles.loginButtonActive,
+          )}
+          onPress={onLogout}>
+          <Text style={styles.loginButtonText}>로그아웃</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonZone: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  loginButton: {
+    backgroundColor: 'gray',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  loginButtonActive: {
+    backgroundColor: 'blue',
+  },
+  loginButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
